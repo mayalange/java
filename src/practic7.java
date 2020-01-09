46a6bc691a6f9a22643c0778ecf67a487413134e
maya_lange <mayalange@mail.ru>
2020-01-09 18:40:28 +0300

    practic №7

---
+++ src/practic7.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class practic7 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите число: ");
+        int number = in.nextInt();
+        int X = 1;
+        int Y = 3;
+        int Z = 7;
+        if (number == X || number == Y || number == Z) {
+            System.out.println("Данное значение имеется в константах");
+                }
+
+                else {
+
+                    System.out.println("Такой константы нет!");
+                }
+            }
+        }
+

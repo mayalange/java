883612237a485c73f203b2302ee0e373c843bb64
maya_lange <mayalange@mail.ru>
2020-01-09 17:29:11 +0300

    practic №5

---
+++ src/practic5.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class practic5 {
+        public static void main (String[] args) {
+            Scanner in = new Scanner(System.in);
+            System.out.print("Введите число: ");
+            int number = in.nextInt();
+            int a = 1;
+            int b = 2;
+            int c = 3;
+            int d = 4;
+            int e = 5;
+            int f = 6;
+            int g = 7;
+            int h = 8;
+            int i = 9;
+            int j = 10;
+            System.out.println(number + "*" + a + "=" + number*a);
+            System.out.println(number + "*" + b + "=" + number*b);
+            System.out.println(number + "*" + c + "=" + number*c);
+            System.out.println(number + "*" + d + "=" + number*d);
+            System.out.println(number + "*" + e + "=" + number*e);
+            System.out.println(number + "*" + f + "=" + number*f);
+            System.out.println(number + "*" + g + "=" + number*g);
+            System.out.println(number + "*" + h + "=" + number*h);
+            System.out.println(number + "*" + i + "=" + number*i);
+            System.out.println(number + "*" + j + "=" + number*j);
+
+        }
+}

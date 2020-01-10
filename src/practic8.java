e6018cf232c63d2f5d87dabfda9707aa69b0fec6
maya_lange <mayalange@mail.ru>
2020-01-10 14:50:06 +0300

    practic №8

---
+++ src/practic8.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class practic8 {
+    public static void main(String[] args) {
+       int x = 1;
+       do {
+           System.out.print(x + "\n");
+           x++;
+       }
+
+        while (x < 51);
+    }
+    }

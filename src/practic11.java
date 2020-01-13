e14d1cab2630562259d91a9ddde0d8887f68354b
maya_lange <mayalange@mail.ru>
2020-01-13 17:14:12 +0300

    practic №11

---
+++ src/practic11.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class practic11 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите значение строки: ");
+        String stroka = in.nextLine();
+        System.out.println("Ваше значение: " + stroka);
+
+    }
+}

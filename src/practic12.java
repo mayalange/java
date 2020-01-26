afe38a8506c36cad645e38a4f46da5dd7b30bd94
maya_lange <mayalange@mail.ru>
2020-01-26 17:26:25 +0300

    practic12

--- src/practic12.java
+++ src/practic12.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class practic12 {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         Scanner in = new Scanner(System.in);
         System.out.print("Введите значение строки: ");
         String stroka = in.nextLine();

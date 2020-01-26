a28ecda20a00c398066652627979e60b226f0c42
maya_lange <mayalange@mail.ru>
2020-01-26 17:27:25 +0300

    practic7

--- src/practic7.java
+++ src/practic7.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class practic7 {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         {
 
             int[] numbers = {10, 20, 30, 40, 50};
